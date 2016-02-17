350a62730fad98e419473e845be74b85cd694c7e
Sofia Celi <sceli@thoughtworks.com>
2016-02-17 15:39:33 -0500

    setup changed

--- Tests/PrimeFactors/PrimeFactorsTest.java
+++ Tests/PrimeFactors/PrimeFactorsTest.java
@@ -10,24 +10,20 @@ import java.util.List;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.*;
 
-/**
- * Created by sceli on 2/17/16.
- */
 public class PrimeFactorsTest {
 
     PrimeFactors primeFactorsTest;
-    List<Integer> testList = new ArrayList<Integer>() {{
-        add(2);
-        add(2);
-        add(2);
-        add(5);
-    }};
-
+    List<Integer> testList;
 
     @Before
     public void setUp() {
         primeFactorsTest = new PrimeFactors();
-
+        testList = new ArrayList<Integer>() {{
+            add(2);
+            add(2);
+            add(2);
+            add(5);
+        }};
     }
 
     @Test
