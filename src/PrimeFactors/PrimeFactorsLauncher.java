81d730c90dbc2f46c65898c2c769a8f05aa7e4bc
Sofia Celi <sceli@thoughtworks.com>
2016-02-18 14:35:37 -0500

    fixed text

--- src/PrimeFactors/PrimeFactorsLauncher.java
+++ src/PrimeFactors/PrimeFactorsLauncher.java
@@ -3,7 +3,8 @@ package PrimeFactors;
 public class PrimeFactorsLauncher {
     public static void main(String[] args) {
         PrimeFactors primeFactorsLauncher = new PrimeFactors();
-            System.out.println("Let's print bla");
+            System.out.println("Let's print all the prime factors of a number:");
             primeFactorsLauncher.generatePrimes(30);
         }
     }
+
